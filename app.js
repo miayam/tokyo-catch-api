62af545fea9c63e242762735e35bc0a0ef54dca1
Muhammad D. R <muhammaddeni90@gmail.com>
2022-03-31 02:38:24 +0700

    feat: Set up test suites.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 const PORT = 3000;
 
-app.use('/', require('./routes'));
+app.use('/', routes);
 
 app.listen(PORT, () => {
   console.log(`Server started at port: ${PORT}`);
