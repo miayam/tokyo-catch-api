91a5fc37fc0e680648b2f2e6d59c2019d41ad7b1
Muhammad D. R <muhammaddeni90@gmail.com>
2022-04-01 22:21:42 +0700

    feat: Add behaviour testing for /users/:userId/rewards.

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
-const PORT = 3000;
-
 app.use('/', routes);
 
-app.listen(PORT, () => {
-  console.log(`Server started at port: ${PORT}`);
-});
+module.exports = app;
--- test/userCreateRewards.test.js
+++ test/userCreateRewards.test.js
@@ -1,5 +1,105 @@
-describe('Sample Test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const app = require('../app');
+
+describe('User get rewards', () => {
+  it('gets /users/:userId', (done) => {
+    request(app)
+      .get('/users/1')
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.userId).toBe('1');
+      })
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it(`
+    gets error when accessing /users/:userId/rewards
+    for the first time without ?at=
+  `, (done) => {
+    request(app)
+      .get('/users/1/rewards') // ?at is not provided
+      .expect("Content-Type", /json/)
+      .expect(422)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it(`
+    gets error when accessing /users/:userId/rewards
+    for the first time with invalid ?at=
+  `, (done) => {
+    request(app)
+      .get('/users/1/rewards?at=fafifuwasweswos') // ?at is not ISO 8061 format
+      .expect("Content-Type", /json/)
+      .expect(422)
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it(`
+    gets rewards data when accessing /users/:userId/rewards
+    for the first time with valid ?at=
+  `, (done) => {
+    // Arrange
+    const iso8061Format = '2020-03-19T12:00:00Z';
+    const url = `/users/1/rewards?at=${iso8061Format}`;
+
+    // Act and assert
+    request(app)
+      .get(url) // ?at is provided
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.length).toBe(7);
+      })
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  it(`
+    gets rewards data when accessing /users/:userId/rewards
+    without ?at for the second time because it has been
+    cached by server
+  `, (done) => {
+    // Arrange
+    const iso8061Format = '2020-03-20T12:00:00Z';
+    const firstAttemptURL = `/users/1/rewards?at=${iso8061Format}`;
+
+    // Request endpoint for the first time.
+    request(app)
+      .get(firstAttemptURL) // ?at is provided
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.length).toBe(7); // Create new rewards
+      })
+
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
+    
+    // Request endpoint for the second time expecting data stored in cache.
+    request(app)
+      .get('/users/1/rewards') // ?at is not provided
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.length).toBe(7); // Get rewards from cache
+      })
+      .end((err) => {
+        if (err) return done(err);
+        return done();
+      });
   });
 });
